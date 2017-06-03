12c441b20c18e8cb75a98cd124575e037fa6710d
Kcatnapper <kchathura12@gmail.com>
2017-06-03 21:41:40 +0530

    added a click listener

--- app/src/main/java/com/testapps/katnapper/githubsearchapp/MainActivity.java
+++ app/src/main/java/com/testapps/katnapper/githubsearchapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import com.testapps.katnapper.githubsearchapp.Github.GithubObject;
@@ -27,14 +28,23 @@ public class MainActivity extends AppCompatActivity implements displayResutls {
     List<String> results = new ArrayList<>();
     ListView list;
     EditText searchText;
+    Button search;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         list = (ListView) findViewById(R.id.lst_results);
         searchText = (EditText) findViewById(R.id.txtSearch);
+        search = (Button) findViewById(R.id.btn_search);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displaySearchResults();
+            }
+        });
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,results);
         list.setAdapter(adapter);
+
     }
 
     public void showResults(JSONObject jsn){
@@ -56,18 +66,13 @@ public class MainActivity extends AppCompatActivity implements displayResutls {
     }
 
 
-    public void displaySearchResults(View view) {
-        Log.v("buttonClick","enterred method");
+    public void displaySearchResults() {
         String search = searchText.getText().toString();
         ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        Log.v("buttonClick","connectivity manager made");
         if(connectivityManager!=null){
-            Log.v("buttonClick","after connectivity manager is made");
             NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
             if(networkInfo != null && networkInfo.isConnected() && (networkInfo.getType()==ConnectivityManager.TYPE_MOBILE || networkInfo.getType()==ConnectivityManager.TYPE_WIFI)){
-                Log.v("buttonClick","connection extablished");
                 new Downloader(this).execute(search);
-                Log.v("buttonClick","running method");
             }
         }
     }
